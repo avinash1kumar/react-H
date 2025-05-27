9fcdba9f4859a66ec3216f876b869c775b48e188
Avinash kumar <avinash954638@gmail.com>
2025-05-28 04:45:21 +0530

    this is first day of react with chai aur code

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1>Chai aur React | Avinash Kumar <br />
+   <br />
+   firstly install react app using vite. <br />
+    so, open terminal and type npm create vite@latest
+    now, Asking for folder name so write your folder name mine is 01vitereact.
+    Now, chose react 
+    Now, chose javascript
+    till this step react app is install in your system but one important file is not install that is node_modules. so, for this type cd your folder name mine is cd 01vitereact
+    Now, npm install or npm i
+    Now, node modules is install.
+    Now, one link will appear so, click on that link to open your first react app with vite
+
+    ......
+    .....
+    <br />
+    Now you have to delete some folder for your better understanding. <br />
+    delete assets <br />
+    delete app.css <br />
+    delete index.css <br />
+
+   </h1>
+  )
+}
+
+export default App
